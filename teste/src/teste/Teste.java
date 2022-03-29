c81be14a284a1d715dfda8fea4d832a59459b8d6
rafaelafj <54038023+rafaelafj@users.noreply.github.com>
2022-03-29 20:59:52 -0300

    Update Teste.java

--- teste/src/teste/Teste.java
+++ teste/src/teste/Teste.java
@@ -2,10 +2,12 @@ package teste;
 
 public class Teste {
   
-  public static void main(String[] args) {
-    int num1 = 3, num2 = 5, soma;
-    soma = num1 + num2;
-  }
-  
+     public static void main(String[] args) {
+     int num1 = 3, num2 = 5, soma;
+      soma = num1 + num2;
+    
+      System.out.println(soma);
+       
+    } 
 
 }
